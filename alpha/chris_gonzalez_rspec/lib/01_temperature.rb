2944070d99b14db5f4825e3910ace56d97121a9f
chrismgonzalez <cmgonza89@gmail.com>
2018-11-13 21:20:07 -0600

    added rspec files

---
+++ alpha/chris_gonzalez_rspec/lib/01_temperature.rb
@@ -0,0 +1,9 @@
+#ftoc
+
+def ftoc(num)
+  (num - 32) * 5.0 / 9
+end
+
+def ctof(num)
+(num * 9.0 / 5) + 32
+end
---
+++ alpha/chris_gonzalez_rspec/lib/02_calculator.rb
@@ -0,0 +1,27 @@
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(arr)
+  if arr.length == 0
+    return 0
+  end
+
+  if arr.length == 1
+    return arr[0]
+  end
+
+  if arr.length == 2
+    return arr[0] + arr[1]
+  end
+
+  if arr.length > 2
+   return arr.sum
+  end
+
+end
+
