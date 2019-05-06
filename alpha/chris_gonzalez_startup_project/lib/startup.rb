2736d43d0f911ff685112bcea195f7ed083f21d1
chrismgonzalez <cmgonza89@gmail.com>
2019-05-05 19:22:04 -0500

    added starup.rb logic

--- alpha/chris_gonzalez_startup_project/lib/startup.rb
+++ alpha/chris_gonzalez_startup_project/lib/startup.rb
@@ -1,6 +1,9 @@
 require "employee"
 
 class Startup
+
+  attr_reader :name, :funding, :salaries, :employees
+
   def initialize(name, funding, salaries)
     @name = name
     @funding = funding
@@ -8,29 +11,45 @@ class Startup
     @employees = []
   end
 
-  def name
-    @name
-  end
-
-  def funding
-    @funding
-  end
-
-  def salaries
-    @salaries
-  end
-
-  def employees
-    @employees
-  end
-
   def valid_title?(title)
-    if @salaries.key?(title)
-      return true
-    end
-    return false
+    @salaries.has_key?(title)
   end
 
-  def 
+  def >(other_startup)
+    self.funding > other_startup.funding
   end
+
+  def hire(employee_name, title)
+    if self.valid_title?(title)
+      @employees << Employee.new(employee_name, title)
+    else
+      raise "Invalid title for employee!"
+    end
+  end
+
+  def size
+     @employees.length
+  end
+
+  def pay_employee(employee)
+    money_owed = @salaries[employee.title]
+
+    if @funding >= money_owed
+      employee.pay(money_owed)
+      @funding -= money_owed
+    else
+      raise "Not enough funds!"
+    end
+
+  end
+
+  def payday
+    @employees.each do |employee|
+      self.pay_employee(employee)
+    end
+  end
+
+
+
 end
+
