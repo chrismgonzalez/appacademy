ef735a9e26a2a512bd8aae8f99399c9a22e080cb
chrismgonzalez <cmgonza89@gmail.com>
2019-05-06 22:22:30 -0500

    added methods

--- alpha/mastermind_project/lib/code.rb
+++ alpha/mastermind_project/lib/code.rb
@@ -5,5 +5,19 @@ class Code
     "B" => :blue,
     "Y" => :yellow
   }
+
+  attr_reader :pegs
+
+  def self.valid_pegs?(chars)
+    chars.all? { |char| POSSIBLE_PEGS.has_key?(char.upcase) }
+  end
+
+  def initialize(chars)
+    if Code.valid_pegs?(chars)
+      @pegs = chars.map(&:upcase)
+    else
+      raise "You pegs are invalid!"
+    end
+  end
   
 end
