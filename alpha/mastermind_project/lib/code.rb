48b6e29dbb8aefccecb38680d7f7e7322a0bb7f2
chrismgonzalez <cmgonza89@gmail.com>
2019-05-06 22:34:50 -0500

    finished part 1

--- alpha/mastermind_project/lib/code.rb
+++ alpha/mastermind_project/lib/code.rb
@@ -12,6 +12,16 @@ class Code
     chars.all? { |char| POSSIBLE_PEGS.has_key?(char.upcase) }
   end
 
+  def self.random(length)
+    random_pegs = []
+    length.times { random_pegs << POSSIBLE_PEGS.keys.sample}
+    Code.new(random_pegs)
+  end
+
+  def self.from_string(string)
+    Code.new(string.split(""))
+  end
+
   def initialize(chars)
     if Code.valid_pegs?(chars)
       @pegs = chars.map(&:upcase)
@@ -19,5 +29,14 @@ class Code
       raise "You pegs are invalid!"
     end
   end
-  
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def length
+    @pegs.length
+  end
+
+
 end
